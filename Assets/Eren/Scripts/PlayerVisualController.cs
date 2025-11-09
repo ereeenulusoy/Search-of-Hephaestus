72a8213b59b73f0bcd377fd84ec6e1604760c5d7
youjirohanma <erenulusoy961@gmail.com>
2025-11-10 01:26:50 +0300

    Sadece Pistolgun ve Pump Olacak

--- Assets/Eren/Scripts/PlayerVisualController.cs
+++ Assets/Eren/Scripts/PlayerVisualController.cs
@@ -1,27 +1,24 @@
 using UnityEngine;
-using UnityEngine.Animations.Rigging;
 
 public class PlayerVisualController : MonoBehaviour
 {
-    [SerializeField] private Rig leftHandRig;
+
     private Animator anim;
     [SerializeField] private Transform[] gunTransform;
 
     [SerializeField] private Transform pistolGun;
     [SerializeField] private Transform shotgun;
-    [SerializeField] private Transform gauntlet;
-    [SerializeField] private Transform pipe;
-    [SerializeField] private Transform trident;
+
 
     private Transform currentGun;
-    
+
     [Header("Left Hand IK")]
     [SerializeField] Transform leftHandTarget;
-    
+
 
     private void Start()
     {
-        SwitchOn(pipe);
+        SwitchOn(pistolGun);
 
         anim = GetComponentInParent<Animator>();
     }
@@ -29,37 +26,20 @@ public class PlayerVisualController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            SwitchOn(pipe);
+            SwitchOn(pistolGun);
             SwitchAnimationLayers(1);
-          
+
         }
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            SwitchOn(pistolGun);
-            SwitchAnimationLayers(2);
-           
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
             SwitchOn(shotgun);
-            SwitchAnimationLayers(3);
-            
+            SwitchAnimationLayers(2);
+
         }
-        if (Input.GetKeyDown(KeyCode.Alpha4))
-        {
-            SwitchOn(gauntlet);
-            SwitchAnimationLayers(4);
-            
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha5))
-        {
-            SwitchOn(trident);
-            SwitchAnimationLayers(5);
-           
-        }
+
     }
 
-   
+
     private void SwitchOn(Transform weaponTransform)
     {
         SwitchOffWeapons();
@@ -80,19 +60,19 @@ public class PlayerVisualController : MonoBehaviour
     private void AttachLeftHand()
     {
         Transform targetTransform = currentGun.GetComponentInChildren<LeftHandTargetPosition>().transform;
-      
+
 
         leftHandTarget.localPosition = targetTransform.localPosition;
         leftHandTarget.localRotation = targetTransform.localRotation;
 
-       
+
     }
 
     private void SwitchAnimationLayers(int layerIndex)
     {
-        for (int i = 1; i <anim.layerCount; i++)
+        for (int i = 1; i < anim.layerCount; i++)
         {
-        anim.SetLayerWeight(i, 0);
+            anim.SetLayerWeight(i, 0);
         }
         anim.SetLayerWeight(layerIndex, 1);
     }
